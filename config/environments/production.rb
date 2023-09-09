f31fcd845bed0a410da8b21925606315bd7c0502
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-09-08 22:40:42 -0500

    Email sender changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,9 +100,9 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => 'apikey',
+    :password       => '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',
+    :domain         => 'eaglevillesoaring.com',
     :enable_starttls_auto => true
   }
   
