cd00882996fafc8eb3e9f8ed8ca9cae33ba9a261
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-09-10 23:28:44 -0500

    Fix bug where new user doesn't auto-create permissions correctly

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,15 +2,15 @@ class Permission < ApplicationRecord
   before_validation :set_defaults
   belongs_to :user
 
-
-
-  private 
-
   def set_defaults
-    user_admin = false if user_admin.blank?
-    club_admin = false if club_admin.blank?
-    site_admin = false if site_admin.blank?
-    global_admin = false if global_admin.blank?
+    self.user_admin ||= false 
+    self.club_admin ||= false
+    self.site_admin ||= false
+    self.global_admin ||= false
   end
 
 end
+
+  private 
+
+  
