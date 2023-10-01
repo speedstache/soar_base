510099a7c7be935830ad82bc3aaf168002e2474c
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-10-01 18:20:28 -0500

    change syntax

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,4 @@ Rails.configuration.stripe = {
   :publishable_key => Rails.application.credentials.stripe[:stripe_public_key]
   :secret_key => Rails.application.credentials.stripe[:stripe_private_key]
 } 
-Stripe.api_key = Rails.application.credentials.stripe[:stripe_private_key]
+Stripe.api_key = Rails.configuration.stripe[:stripe_private_key]
