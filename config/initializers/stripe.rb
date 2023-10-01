ecd636fa4feda5a2bd968518daac0a42c9e427de
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-10-01 18:33:48 -0500

    initializer again

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,4 @@ Rails.configuration.stripe = {
   :publishable_key => Rails.application.credentials.dig(:stripe_public_key),
   :secret_key => Rails.application.credentials.dig(:stripe_private_key)
 } 
-Stripe.api_key = Rails.configuration.dig(:stripe_private_key)
+Stripe.api_key = Rails.configuration.stripe(:secret_key)
