0d8c2194010e218f378e5687d7a8fc8a2ad77d58
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-10-01 18:27:19 -0500

    adjust stripe initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = { 
-  :publishable_key => Rails.application.credentials.stripe[:stripe_public_key],
-  :secret_key => Rails.application.credentials.stripe[:stripe_private_key]
+  :publishable_key => Rails.application.credentials.dig[:stripe, :stripe_public_key],
+  :secret_key => Rails.application.credentials.dig[:stripe, :stripe_private_key]
 } 
-Stripe.api_key = Rails.configuration.stripe[:stripe_private_key]
+Stripe.api_key = Rails.configuration.dig[:stripe, :stripe_private_key]
