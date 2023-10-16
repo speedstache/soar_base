910a48c92a1da26885b5402115109542b01d4604
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-10-16 00:45:00 -0500

    improved user profile show page, need to de-duplicate routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1 or /users/1.json
   def show
     @user = User.find(params[:id])
-    @membership_info = current_user.memberships.last
-    @my_membership = current_user.membership_users.last
+    @membership_info = @user.memberships.last
+    @my_membership = @user.membership_users.last
     @my_renewal_date = @my_membership.renewal_date + @membership_info.renewal_period
   end
 
