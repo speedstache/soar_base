0ba94a1411dd51875e65b5dbcf35073d1efd15da
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-11-12 23:02:48 -0600

    changed order of initializers

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = Rails.application.credentials.recaptcha_v2.site_key ||= ENV['RECAPTCHA_SITE_KEY']
-  config.secret_key = Rails.application.credentials.recaptcha_v2.secret_key ||= ENV['RECAPTCHA_SECRET_KEY']
+  config.site_key = ENV['RECAPTCHA_SITE_KEY'] ||= Rails.application.credentials.recaptcha_v2.site_key
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY'] ||= Rails.application.credentials.recaptcha_v2.secret_key
 end
