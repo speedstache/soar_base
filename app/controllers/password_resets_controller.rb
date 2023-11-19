0711f94af790120b6e0d7aa46062908b48620773
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-11-18 22:38:58 -0600

    update password reset mail template

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,7 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:notice] = "Email sent with password reset instructions"
+      flash[:success] = "Email sent with password reset instructions"
       redirect_to root_url
     else
       flash.now[:danger] = "Email address not found"
