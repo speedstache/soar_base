746ea4d832c215d229821ddc36b6c613da31d843
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-12-14 21:04:18 -0600

    updated template for field status

--- app/mailers/field_status_mailer.rb
+++ app/mailers/field_status_mailer.rb
@@ -2,7 +2,9 @@ class FieldStatusMailer < ApplicationMailer
 
   def field_status(field_status_update)
     @field_status_update = field_status_update
-    mail to: "nathan.r.taylor@gmail.com", subject: "Eagleville Soaring Club - Field Operation Status"
+    @flying = @field_status_update.ops_status ? 'Yes' : 'No'
+
+    mail to: "nathan.r.taylor@gmail.com", subject: "Eagleville Soaring Club - Are we Flying? "+@flying
   end
   
 end
