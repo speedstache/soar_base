4d3c6e49411913d21f11b13170d029f6e6927a56
Nathan Taylor <nathan.r.taylor@gmail.com>
2023-12-22 21:25:21 -0600

    made pagination easier to see in css

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -4,7 +4,7 @@ class Aircraft < ApplicationRecord
   validates_uniqueness_of :name , scope: [ :short_name ]
   validates :group, inclusion: { in: [ "club", "private", "towplane", "instructor" ], allow_blank: true, message: "not an allowable aircraft type" }
   has_many :aircraft_users
-  has_many :users, through: :aircraft_users
+  has_many :users, through: :aircraft_usersr
   has_many :reservations
   has_many :flights
   has_many :tows
