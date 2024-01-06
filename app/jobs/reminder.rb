f6fb88ad3b9109246749c8a3813fe46ae962b872
Nathan Taylor <nathan.r.taylor@gmail.com>
2024-01-05 21:58:31 -0600

    change date query for reminder emails

--- app/jobs/reminder.rb
+++ app/jobs/reminder.rb
@@ -2,7 +2,7 @@ class Reminder < ApplicationJob
   queue_as :default
 
   def perform
-    reservations = Reservation.where(reservation_date: Date.today..1.day.from_now)
+    reservations = Reservation.where(reservation_date: Date.today..0.day.from_now)
 
       reservations.each do |reservation|
       ReservationMailer.reminder(reservation).deliver_later
