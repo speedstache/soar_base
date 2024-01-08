87fd5c4460a2123110d43915178e05a60bf7f841
Nathan Taylor <nathan.r.taylor@gmail.com>
2024-01-07 20:28:24 -0600

    fixed next day reminder date range

--- app/jobs/reminder.rb
+++ app/jobs/reminder.rb
@@ -2,7 +2,7 @@ class Reminder < ApplicationJob
   queue_as :default
 
   def perform
-    reservations = Reservation.where(reservation_date: Date.today..0.day.from_now)
+    reservations = Reservation.where(reservation_date: 1.day.from_now(Time.now))
 
       reservations.each do |reservation|
       ReservationMailer.reminder(reservation).deliver_later
--- app/sidekiq/reservation_reminder_job.rb
+++ app/sidekiq/reservation_reminder_job.rb
@@ -4,7 +4,7 @@ class ReservationReminderJob
   include Sidekiq::Worker
 
   def perform
-    reservations = Reservation.where(reservation_date: Date.today..1.day.from_now)
+    reservations = Reservation.where(reservation_date: 1.day.from_now(Time.now))
 
       reservations.each do |reservation|
       ReservationMailer.reminder(reservation).deliver_later
