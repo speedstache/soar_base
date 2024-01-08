e7d18c0839a563785a53fb30822ac3c6cc549cec
Nathan Taylor <nathan.r.taylor@gmail.com>
2024-01-07 19:57:14 -0600

    update no fly day job

--- app/jobs/reminder.rb
+++ app/jobs/reminder.rb
@@ -8,25 +8,4 @@ class Reminder < ApplicationJob
       ReservationMailer.reminder(reservation).deliver_later
       end
 
-    #automatically change status of reservations on days where field status indicates no flying to 'noflyday'  
-    if FieldStatusUpdate.where(date: Date.today).present?  
-      noflystatus = FieldStatusUpdate.where(date: Date.today).last
-        if noflystatus.ops_status == false
-
-          noflyreservations = Reservation.where(reservation_date: noflystatus.date)
-
-          noflyreservations.each do |noflyreservation|
-            noflyreservation.update(status: 'noflyday')
-            noflyreservation.save
-
-          end
-        else
-          #if latest FieldStatusUpdate is true, we're flying and won't change any reservation statuses
-        end
-    else
-      #if there are no FieldStatusUpdates for today, don't run this update
-    end
-
-  end
-
 end 
---
+++ app/sidekiq/noflyday_updater.rb
@@ -0,0 +1,29 @@
+require 'sidekiq-scheduler'
+
+class NoFlyDayJob
+  include Sidekiq::Worker
+
+  def perform
+
+    #automatically change status of reservations on days where field status indicates no flying to 'noflyday'  
+    if FieldStatusUpdate.where(date: Date.today).present?  
+      noflystatus = FieldStatusUpdate.where(date: Date.today).last
+        if noflystatus.ops_status == false
+
+          noflyreservations = Reservation.where(reservation_date: noflystatus.date)
+
+          noflyreservations.each do |noflyreservation|
+            noflyreservation.update(status: 'noflyday')
+            noflyreservation.save
+
+          end
+        else
+          #if latest FieldStatusUpdate is true, we're flying and won't change any reservation statuses
+        end
+    else
+      #if there are no FieldStatusUpdates for today, don't run this update
+    end
+
+  end
+  
+end
